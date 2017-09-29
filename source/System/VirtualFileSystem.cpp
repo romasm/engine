16e60bb9a9614fb8e1fc96e135e28b8eba3d154a
romasm <romasm@users.noreply.github.com>
2017-09-29 20:19:06 +0300

    virtual file system

--- source/System/VirtualFileSystem.cpp
+++ source/System/VirtualFileSystem.cpp
@@ -9,14 +9,18 @@ Package::Package()
 {
 	packedData = nullptr;
 	packedSize = 0;
+	fileDataOffset = 0;
 	unwrapped = true;
+	fullyLoaded = false;
 }
 
 Package::Package(string& path)
 {
 	packedData = nullptr;
 	packedSize = 0;
+	fileDataOffset = 0;
 	unwrapped = true;
+	fullyLoaded = false;
 
 	ReadFromDisk(path);
 }
@@ -26,28 +30,159 @@ Package::~Package()
 	Clear();
 }
 
+void Package::CalcDataSize(uint32_t* size, unordered_map<string, HNode>* content)
+{
+	for(auto& it: *content)
+	{
+		if(it.second.file.date == 0)
+			continue;
+
+		if(it.second.content)
+			CalcDataSize(size, it.second.content);
+		else
+			size += it.second.file.size;
+	}
+}
+
+void Package::MoveData(uint8_t* newData, uint32_t newSize, uint32_t* dataOffset, unordered_map<string, HNode>* content)
+{
+	for(auto& it: *content)
+	{
+		if(it.second.file.date == 0)
+			continue;
+
+		if(it.second.content)
+		{
+			MoveData(newData, newSize, dataOffset, it.second.content);
+			continue;
+		}
+
+		if( *dataOffset + it.second.file.size > newSize )
+		{
+			ERR("Package %s buffer overflow!", pkgName.data());
+			return;
+		}
+
+		if(it.second.file.dataPointer)
+		{
+			memcpy(newData + *dataOffset, it.second.file.dataPointer, it.second.file.size);
+			_DELETE_ARRAY(it.second.file.dataPointer);
+		}
+		else
+		{
+			memcpy(newData + *dataOffset, packedData + it.second.file.offset, it.second.file.size);
+		}
+
+		it.second.file.offset = *dataOffset;
+		*dataOffset += it.second.file.size;
+	}
+}
+
+void Package::ClearDeleted(unordered_map<string, HNode>* content)
+{
+	for(auto& it: *content)
+	{
+		if(it.second.file.date == 0)
+		{
+			ClearNode(&it.second);
+		}
+		else
+		{
+			if(it.second.content)
+				ClearDeleted(it.second.content);
+		}
+	}
+}
+
 void Package::WrapUp()
 {
-	// TODO
+	if(!unwrapped)
+		return;
+
+	if(!fullyLoaded)
+	{
+		ERR("Only fully loaded packages can be modified");
+		return;
+	}
+
+	uint32_t newSize = packedSize;
+	CalcDataSize(&newSize, &pkgMap);
+	if( newSize == 0 )
+	{
+		LOG("No content in package %s", pkgName.data());
+		Clear();
+		return;
+	}
+
+	uint8_t* newData = new uint8_t[newSize];
+	uint32_t dataOffset = 0;
+	MoveData(newData, newSize, &dataOffset, &pkgMap);
+
+	if( dataOffset != newSize )
+		WRN("Something went wrong during data wraping up for %s", pkgName.data());
+
+	_DELETE_ARRAY(packedData);
+	packedData = newData;
+	packedSize = newSize;
+
+	ClearDeleted(&pkgMap);
 
 	unwrapped = false;
+}
+
+void Package::ClearNode(HNode* node)
+{
+	_DELETE_ARRAY(node->file.dataPointer);
+	if(!node->content)
+		return;
+
+	for(auto& it: *(node->content))
+	{
+		ClearNode(&it.second);
+	}
+	_DELETE(node->content);
 }
 
 void Package::Clear()
 {
 	for(auto& it: pkgMap)
-	{
-		_DELETE_ARRAY(it.second.dataPointer);
-	}
+		ClearNode(&it.second);
 
 	_DELETE_ARRAY(packedData);
 	packedSize = 0;
 
 	pkgMap.clear();
-	pkgName.clear();
-	pkgPath.clear();
-
 	unwrapped = true;
+}
+
+void Package::ReadNode(uint8_t** t_data, unordered_map<string, HNode>* content)
+{
+	uint32_t nodesCount = *(uint32_t*)(*t_data);
+	*t_data += sizeof(uint32_t);
+
+	for(uint32_t i = 0; i < nodesCount; i++)
+	{
+		uint32_t stringSize = *(uint32_t*)(*t_data);
+		*t_data += sizeof(uint32_t);
+		string nodeName((char*)(*t_data), stringSize);
+		*t_data += sizeof(char) * stringSize;
+
+		HNode node;
+		node.file.date = *(uint32_t*)(*t_data);
+		*t_data += sizeof(uint32_t);
+		node.file.offset = *(uint32_t*)(*t_data);
+		*t_data += sizeof(uint32_t);
+		node.file.size = *(uint32_t*)(*t_data);
+		*t_data += sizeof(uint32_t);
+
+		if(node.file.size == 0)
+		{
+			node.content = new unordered_map<string, HNode>;
+			ReadNode(t_data, node.content);
+		}
+
+		content->insert(make_pair(nodeName, node));
+	}
 }
 
 bool Package::ReadFromDisk(string& path)
@@ -75,28 +210,12 @@ bool Package::ReadFromDisk(string& path)
 	}
 	t_data += sizeof(uint32_t);
 
-	uint32_t filesCount = *(uint32_t*)t_data;
+	ReadNode(&t_data, &pkgMap);
+
+	fileDataOffset = *(uint32_t*)t_data;
 	t_data += sizeof(uint32_t);
 
-	for(uint32_t i = 0; i < filesCount; i++)
-	{
-		uint32_t stringSize = *(uint32_t*)t_data;
-		t_data += sizeof(uint32_t);
-		string fileName((char*)t_data, stringSize);
-		t_data += sizeof(char) * stringSize;
-
-		FileLink fileLink;
-		fileLink.date = *(uint32_t*)t_data;
-		t_data += sizeof(uint32_t);
-		fileLink.offset = *(uint32_t*)t_data;
-		t_data += sizeof(uint32_t);
-		fileLink.size = *(uint32_t*)t_data;
-		t_data += sizeof(uint32_t);
-
-		pkgMap.insert(make_pair(fileName, fileLink));
-	}
-
-	uint32_t packedSize = *(uint32_t*)t_data;
+	packedSize = *(uint32_t*)t_data;
 	t_data += sizeof(uint32_t);
 
 	packedData = new uint8_t[packedSize];
@@ -107,9 +226,75 @@ bool Package::ReadFromDisk(string& path)
 	pkgPath = path;
 	pkgName = RemoveExtension(GetFilename(path));
 	unwrapped = false;
+	fullyLoaded = true;
 
 	LOG_GOOD("Package %s loaded from disk", path.data());
 	return true;
+}
+
+void Package::CalcNodeSize(uint32_t* size, unordered_map<string, HNode>* content)
+{
+	*size += sizeof(uint32_t);
+	for(auto& it: *content)
+	{
+		if( it.second.file.size == 0 && !it.second.content )
+		{
+			ERR("Zero size files is not supported for package %s", pkgName.data());
+			continue;
+		}
+
+		if( it.second.file.size != 0 && it.second.content )
+		{
+			WRN("Directory in %s cant be file at same time, file part is ignoring", pkgName.data());
+		}
+
+		size += sizeof(uint32_t) + (uint32_t)it.first.size() * sizeof(char);
+		size += sizeof(uint32_t) + sizeof(uint32_t) + sizeof(uint32_t);
+		
+		if(it.second.content)
+		{
+			CalcNodeSize(size, it.second.content);
+		}
+	}
+}
+
+void Package::WriteNode(uint8_t** t_data, unordered_map<string, HNode>* content)
+{
+	*(uint32_t*)(*t_data) = (uint32_t)content->size();
+	*t_data += sizeof(uint32_t);
+
+	for(auto& it: *content)
+	{
+		if( it.second.file.size == 0 && !it.second.content )
+			continue;
+
+		*(uint32_t*)(*t_data) = (uint32_t)it.first.size();
+		*t_data += sizeof(uint32_t);
+
+		memcpy(*t_data, it.first.data(), it.first.size());
+		*t_data += it.first.size() * sizeof(char);
+
+		if( !it.second.content )
+		{
+			*(uint32_t*)(*t_data) = it.second.file.date;
+			*t_data += sizeof(uint32_t);
+			*(uint32_t*)(*t_data) = it.second.file.offset;
+			*t_data += sizeof(uint32_t);
+			*(uint32_t*)(*t_data) = it.second.file.size;
+			*t_data += sizeof(uint32_t);
+		}
+		else
+		{
+			*(uint32_t*)(*t_data) = 0;
+			*t_data += sizeof(uint32_t);
+			*(uint32_t*)(*t_data) = 0;
+			*t_data += sizeof(uint32_t);
+			*(uint32_t*)(*t_data) = 0;
+			*t_data += sizeof(uint32_t);
+
+			WriteNode(t_data, it.second.content);
+		}
+	}
 }
 
 bool Package::WriteToDisk()
@@ -120,16 +305,17 @@ bool Package::WriteToDisk()
 		return false;
 	}
 
-	if(unwrapped)
-		WrapUp();
+	WrapUp();
 
-	uint32_t fileSize = sizeof(uint32_t) + sizeof(uint32_t);
-	for(auto& it: pkgMap)
-	{
-		fileSize += sizeof(uint32_t) + (uint32_t)it.first.size() * sizeof(char);
-		fileSize += sizeof(uint32_t) + sizeof(uint32_t) + sizeof(uint32_t);
-	}
-	fileSize += sizeof(uint32_t) + packedSize;
+	if(IsEmpty())
+		return false;
+
+	uint32_t fileSize = sizeof(uint32_t);
+	CalcNodeSize(&fileSize, &pkgMap);
+	fileSize += sizeof(uint32_t) + sizeof(uint32_t);
+	
+	fileDataOffset = fileSize;
+	fileSize += packedSize;
 
 	uint8_t* data = new uint8_t[fileSize];
 	uint8_t* t_data = data;
@@ -137,26 +323,10 @@ bool Package::WriteToDisk()
 	*(uint32_t*)t_data = PACKAGE_FILE_VERSION;
 	t_data += sizeof(uint32_t);
 	
-	*(uint32_t*)t_data = (uint32_t)pkgMap.size();
+	WriteNode(&t_data, &pkgMap);
+
+	*(uint32_t*)t_data = fileDataOffset;
 	t_data += sizeof(uint32_t);
-
-	for(auto& it: pkgMap)
-	{
-		*(uint32_t*)t_data = (uint32_t)it.first.size();
-		t_data += sizeof(uint32_t);
-
-		memcpy(t_data, it.first.data(), it.first.size());
-		t_data += it.first.size() * sizeof(char);
-		
-		*(uint32_t*)t_data = it.second.date;
-		t_data += sizeof(uint32_t);
-
-		*(uint32_t*)t_data = it.second.offset;
-		t_data += sizeof(uint32_t);
-
-		*(uint32_t*)t_data = it.second.size;
-		t_data += sizeof(uint32_t);
-	}
 
 	*(uint32_t*)t_data = packedSize;
 	t_data += sizeof(uint32_t);
--- source/System/VirtualFileSystem.h
+++ source/System/VirtualFileSystem.h
@@ -23,28 +23,54 @@ public:
 	bool ReadFromDisk(string& path);
 	bool WriteToDisk();
 
+	bool AddFile(string& path, uint8_t* data, uint32_t size); // TODO
+	bool AddDir(string& path); // TODO
+	bool DeleteContent(string& path); // TODO
+
+	bool IsEmpty() const {return packedSize == 0;};
+
 private:
-	void Clear();
 
 	struct FileLink
 	{
-		uint32_t date;
+		uint32_t date;	// if 0, then deleted
 		uint32_t offset;
-		uint32_t size;
+		uint32_t size;	// if 0, then directory
 		uint8_t* dataPointer;
 
 		FileLink() : date(0), offset(0), size(0), dataPointer(nullptr) {}
 	};
 
+	struct HNode
+	{
+		unordered_map<string, HNode>* content;
+		FileLink file;
+
+		HNode() : content(nullptr) {}
+	};
+
+	void Clear();
+	void ClearNode(HNode* node);
+
+	void ReadNode(uint8_t** t_data, unordered_map<string, HNode>* content);
+	void CalcNodeSize(uint32_t* size, unordered_map<string, HNode>* content);
+	void WriteNode(uint8_t** t_data, unordered_map<string, HNode>* content);
+
+	void CalcDataSize(uint32_t* size, unordered_map<string, HNode>* content);
+	void MoveData(uint8_t* newData, uint32_t newSize, uint32_t* dataOffset, unordered_map<string, HNode>* content);
+	void ClearDeleted(unordered_map<string, HNode>* content);
+
 	string pkgName;
 	string pkgPath;
 
-	unordered_map<string, FileLink> pkgMap;
+	unordered_map<string, HNode> pkgMap;
 
+	uint32_t fileDataOffset; // if 0, then no file written
 	uint32_t packedSize;
 	uint8_t* packedData;
 
 	bool unwrapped;
+	bool fullyLoaded;
 
 public:
 
